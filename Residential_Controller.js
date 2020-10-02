9d750f873a46593ef59fdb0e4a8e952bd17ca453
ibrahimdhouib <70654077+ibrahimdhouib@users.noreply.github.com>
2020-10-02 07:36:35 -0400

    Commit 02/10/2020

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,3 +1,18 @@
+class column{
+    constructor(NumberOfFloors, NumberOfElevators){
+        this.NumberOfFloors = NumberOfFloors;
+        this.numbersofelevators = NumberOfElevators;
+        this.callbutton = [];
+        this.numbersofcallbutton = ((NumberOfFloors*2)-2);
+        this.elevatorList = [];
+
+        for (let i=0; i < NumberOfElevators; i++){
+            let elevator = new Elevator(i, "IDLE", 1, null, "CLOSED")
+            this.elevatorlist.push(elevator)
+        }
+        console.log('Column created', this);
+    }
+}
 
 class ElevatorController {
     constructor(NumbersOfFloors, NumbersOElevators){
@@ -25,7 +40,7 @@ class ElevatorController {
             else if (elevator.Status === "IDLE" && elevator.floor === CurrentFloor){
                 return bestelevator(elevator, Currentdirection, CurrentFloor)
             }
-            else if (elevator.Floor < CurrentFloor && (elevator.Status === "MOVING" || "STOPPED") && elevator.Direction === Currentdirection){  
+            else if (elevator.Floor < CurrentFloor && (elevator.Status === "MOVING" || "STOPPED") && elevator.Direction === Currentdirection){
                 return bestelevator(elevator, Currentdirection, CurrentFloor)
             }
             else if (elevator.Floor > CurrentFloor && (elevator.Status === "MOVING" || "STOPPED") && elevator.Direction === Currentdirection){
@@ -38,5 +53,34 @@ class ElevatorController {
     }
 }
 
+class Elevator {
+    constructor (ElevatorN, status, elevator_floor, elevator_currentDirection, Doorstatus){
+        this.ElevatorN = ElevatorN;
+        this.status = status;
+        this.elevator_floor = elevator_floor;
+        this.floorList = [];
+        this.elevator_currentDirection = elevator_currentDirection;
+        this.doorStatus = doorStatus;
+        this.requestfloorbutton = []
+    }
+
+    sendRequest(RequestFloor){
+        console.log('send request', RequestFloor);
+        this.floorList.push(RequestFloor);
+        this.sort_addFloorList();
+        this.controlelevator(RequestFloor);
+    }
+
+    addFloorList(floor_list){
+        console.log("Entered FloorList in elevator");
+        if (this.elevator_currentDirection === "UP") {
+            this.floor_list.sort();
+        }
+        else if (this.elevator_currentDirection === "DOWN") {
+            this.floor_list.sort().reverse();
+        }
+        let Newfloorlist = FloorList;
+        return Newfloorlist;
+    }
 
 
